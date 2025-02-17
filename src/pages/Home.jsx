140e71e06516499ee11f098af639235e3cf896d3
Namouke <79513377+Namouke@users.noreply.github.com>
2025-02-17 18:11:49 +0100

    import des visuels provisoirs dans la page home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import Location from "../components/location search bar/LocationSearchBar"
+import Carousels from "../components/carousels/Carousels";
+import WeatherPerHour from "../components/weather per hour/WeatherPerHour";
 
 const Home = () => {
     return (
         <section>
             <Location />
+            <Carousels />
+            <WeatherPerHour />
         </section>
     )
 }
